8469b64ee1f25e6764c05060b17d0615dec1dfb3
Yunus Emre <yunus1481emre@gmail.com>
2024-11-06 21:57:18 +0300

    update

--- src/services/const.ts
+++ src/services/const.ts
@@ -1,3 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://rickandmortyapi.com/api";
 
 export const CHARACTER = "/character";
