cb4e3e21f4535607eeef9e41e20cdadcb20e1e14
Yunus Emre <yunus1481emre@gmail.com>
2024-11-06 22:09:23 +0300

    update

--- src/app/client-pages/Home/CharacterCard.tsx
+++ src/app/client-pages/Home/CharacterCard.tsx
@@ -8,17 +8,17 @@ interface CharacterCardProps {
 
 const CharacterCard = ({ character }: CharacterCardProps) => {
   return (
-    <div className="bg-[#3c3e44] flex flex-col md:flex-row rounded-[9px] cursor-pointer gap-[15px]">
-      <div>
+    <div className="bg-[#3c3e44] flex flex-col md:flex-row rounded-[9px] cursor-pointer gap-[15px] p-[20px] md:p-0">
+      <div className="">
         <Image
           src={character.image}
           alt={character.name}
           width={229}
           height={220}
-          className="aspect-[1/1] rounded-l-[8px] md:min-w-[229px] md:min-h-[220px]"
+          className="aspect-[1/1] m-auto md:rounded-l-[8px] md:max-w-[300px] md:max-h-[500px]"
         />
       </div>
-      <div className="flex flex-col justify-between py-[10px] pl-[20px] md:pl-0">
+      <div className="flex flex-col justify-between py-[10px] pl-[20px] md:pl-0 m-auto md:m-0">
         <div className="flex flex-col">
           <span className="text-white hover:text-orange-500">
             {character.name}
